e42b47178e9b428dceb187747fbbc4f12e1540f7
blumemond10 <45660561+blumemond10@users.noreply.github.com>
2020-12-09 11:06:21 +0900

    Initial commit

---
+++ Assets/BallScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 20.0f;
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().AddForce(
+            (transform.forward + transform.right) * speed,
+            ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/BlockScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //衝突判定
+        if(collision.gameObject.tag == "Ball")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
